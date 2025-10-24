6841a8e1fb59432161aa2b2d2be76cebac86dc6c
askhathapoojary <akshathapoojary1110@gmail.com>
2025-10-24 10:17:28 +0530

    first push

---
+++ source_code/basic.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void sum_of(int a , int b)
+{
+    int sum = a+b;
+    cout<<"sum="<<sum<<endl;
+}
+
+int main()
+{
+    cout<<"hi"<<endl;
+    sum_of(3,8);
+}
